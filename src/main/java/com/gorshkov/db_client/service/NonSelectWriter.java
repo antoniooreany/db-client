c66e1e779d8d29bafa81f9b166369ad275859e90
unknown <antonio_oreany@mail.ru>
2022-02-11 21:47:09 +0200

    INSERT, UPDATE, DELETE operation is implemented.

--- src/main/java/com/gorshkov/db_client/service/NonSelectWriter.java
+++ src/main/java/com/gorshkov/db_client/service/NonSelectWriter.java
@@ -1,7 +1,9 @@
 package com.gorshkov.db_client.service;
 
 public class NonSelectWriter {
-
+//  INSERT INTO db.persons VALUES (3, 'value2', 'value3', 'value4', 'value5');
+//  UPDATE db.persons SET PersonID = 55 WHERE PersonID = 3;
+//  DELETE FROM db.persons WHERE PersonID = 55;
     public void write(int rowsChanged) {
         System.out.println("There are " + rowsChanged + " rows changed.");
     }
